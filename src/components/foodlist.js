64037c95fbf3cbe52a45d15b25c93e23dc047ade
codic-yeeshu <yeeshu.exploresmore@gmail.com>
2024-11-21 18:40:09 +0530

    completed the task

--- src/components/foodlist.js
+++ src/components/foodlist.js
@@ -2,60 +2,94 @@ import React, { useState } from "react";
 import "../styles/App.css";
 
 function FoodList() {
-	const [foods, setFoods] = useState([]);
-	const [itemName, setItemName] = useState("");
-	const [foodType, setFoodType] = useState("");
-	const [spicinessLevel, setSpicinessLevel] = useState("");
-	const [isFirstCardEnabled, setIsFirstCardEnabled] = useState(false);
-	const [isSecondCardEnabled, setIsSecondCardEnabled] = useState(false);
-	const [isFormEnabled, setIsFormEnabled] = useState(false);
+  const [foods, setFoods] = useState([]);
+  const [itemName, setItemName] = useState("");
+  const [foodType, setFoodType] = useState("");
+  const [spicinessLevel, setSpicinessLevel] = useState("");
+  const [isFirstCardEnabled, setIsFirstCardEnabled] = useState(false);
+  const [isSecondCardEnabled, setIsSecondCardEnabled] = useState(false);
+  const [isFormEnabled, setIsFormEnabled] = useState(false);
+  const addItem = () => {
+    setFoods((prev) => [...prev, { itemName, foodType, spicinessLevel }]);
+    setFoodType("");
+    setItemName("");
+    setSpicinessLevel("");
+    setIsFirstCardEnabled(false);
+    setIsSecondCardEnabled(false);
+    setIsFormEnabled(false);
+  };
 
-	return (
-		<>
-			<div className="container">
-				<h1>Food Items List</h1>
-				<button>Add Food</button>
+  const deleteItem = (index) => {
+    const newFood = foods.filter((food, idx) => idx !== index);
+    setFoods([...newFood]);
+  };
 
-				<div className="card-container">
-                        <>
-							<h2>Item Name:</h2>
-							<input
-								name="itemName"
-								type="text"
-								disabled={!isFirstCardEnabled}
-							/>
-							<h2>Food Type:</h2>
-							<input
-								name="foodType"
-								type="text"
-								disabled={!isFirstCardEnabled}
-							/>
-							<div className={`card`}>
-								<form>
-									<h2>Spiciness Level:</h2>
-									<input
-										name="spicinessLevel"
-										type="text"
-										disabled={!isFormEnabled}
-									/>
-								</form>
-							</div>
-						</>
-				</div>
-                <div className={`card ${isSecondCardEnabled ? "" : "disabled"}`}>
-						<button>Save</button>
-				</div>
+  return (
+    <>
+      <div className="container">
+        <h1>Food Items List</h1>
+        <button
+          onClick={() => {
+            setIsFirstCardEnabled(true);
+            setIsSecondCardEnabled(true);
+          }}
+        >
+          Add Food
+        </button>
+        {isFirstCardEnabled && (
+          <div className="card-container">
+            <>
+              <h2>Item Name:</h2>
+              <input
+                name="itemName"
+                type="text"
+                disabled={!isFirstCardEnabled}
+                onChange={(e) => setItemName(e.target.value)}
+              />
+              <h2>Food Type:</h2>
+              <input
+                name="foodType"
+                type="text"
+                disabled={!isFirstCardEnabled}
+                onChange={(e) => setFoodType(e.target.value)}
+              />
+              <div className={`card`}>
+                <form onClick={() => setIsFormEnabled(true)}>
+                  <h2>Spiciness Level:</h2>
+                  <input
+                    name="spicinessLevel"
+                    type="text"
+                    disabled={!isFormEnabled}
+                    style={{ opacity: isFormEnabled ? 1 : 0.5 }}
+                    onChange={(e) => setSpicinessLevel(e.target.value)}
+                  />
+                </form>
+              </div>
+            </>
+          </div>
+        )}
+        {isFirstCardEnabled && (
+          <div className={`card ${isSecondCardEnabled ? "" : "disabled"}`}>
+            <button onClick={addItem} disabled={!isSecondCardEnabled}>
+              Save
+            </button>
+          </div>
+        )}
 
-				<ul className="list">
-                        <li>
-							{itemName} ({foodType}) - Spiciness Level:{" "}
-							{spicinessLevel}
-							<button>Delete</button>
-						</li>
-				</ul>
-			</div>
-		</>
-	);
+        <ul className="list">
+          {foods.map((food, index) => {
+            return (
+              <li key={index}>
+                {food.itemName} ({food.foodType}) - Spiciness Level:{" "}
+                {food.spicinessLevel}
+                <button onClick={() => deleteItem(index)}>Delete</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default FoodList;
